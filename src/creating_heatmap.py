3b5db682c646cfef9b062c184361af785a4984c8
martadelfino <martadelfino23@gmail.com>
2023-02-27 11:28:32 +0000

    Fixing indentation

--- src/creating_heatmap.py
+++ src/creating_heatmap.py
@@ -36,50 +36,50 @@ def heatmap(results_pairwise_LD, rsID_list):
     Outputs: the 6 heatmaps (R^2 and D' for British, Nigerian, Japanese populations)
     '''
     ####### ALL THE NECESSERY OUTPUTS FOR THE HEATMAPS. 
-            # BRITISH R2, NIGERIAN R2, JAPANESE R2
-            # BRITISH D', NIGERIAN D', JAPANESE D' 
-            #  
-            # extracting information from the list of dictionaries 
-            # this obtains the r2 values list
-            british_r2_values = [d['british_r2'] for d in results_pairwise_LD]
-            nigerian_r2_values = [d['nigerian_r2'] for d in results_pairwise_LD]
-            japanese_r2_values = [d['japanese_r2'] for d in results_pairwise_LD]
-            british_dprime_values = [d['british_Dprime'] for d in results_pairwise_LD]
-            nigerian_dprime_values = [d['nigerian_Dprime'] for d in results_pairwise_LD]
-            japanese_dprime_values = [d['japanese_Dprime'] for d in results_pairwise_LD]
+    # BRITISH R2, NIGERIAN R2, JAPANESE R2
+    # BRITISH D', NIGERIAN D', JAPANESE D' 
+    #  
+    # extracting information from the list of dictionaries 
+    # this obtains the r2 values list
+    british_r2_values = [d['british_r2'] for d in results_pairwise_LD]
+    nigerian_r2_values = [d['nigerian_r2'] for d in results_pairwise_LD]
+    japanese_r2_values = [d['japanese_r2'] for d in results_pairwise_LD]
+    british_dprime_values = [d['british_Dprime'] for d in results_pairwise_LD]
+    nigerian_dprime_values = [d['nigerian_Dprime'] for d in results_pairwise_LD]
+    japanese_dprime_values = [d['japanese_Dprime'] for d in results_pairwise_LD]
 
-            #### TITLES FOR EACH HEATMAP
-            british_r2_title = 'british_r2_values'
-            nigerian_r2_title = 'nigerian_r2_values'
-            japanese_r2_title = 'japanese_r2_values'
-            british_dprime_title = 'british_dprime_values'
-            nigerian_dprime_title = 'nigerian_dprime_values'
-            japanese_dprime_title = 'japanese_dprime_values'
+    #### TITLES FOR EACH HEATMAP
+    british_r2_title = 'british_r2_values'
+    nigerian_r2_title = 'nigerian_r2_values'
+    japanese_r2_title = 'japanese_r2_values'
+    british_dprime_title = 'british_dprime_values'
+    nigerian_dprime_title = 'nigerian_dprime_values'
+    japanese_dprime_title = 'japanese_dprime_values'
 
-            ### BRITISH R2 HEATMAP
-            british_r2_heatmap = LDheatmap(british_r2_values, rsID_list, british_r2_title)
-            plt.savefig("static/british_r2.png")
+    ### BRITISH R2 HEATMAP
+    british_r2_heatmap = LDheatmap(british_r2_values, rsID_list, british_r2_title)
+    plt.savefig("static/british_r2.png")
 
-            ### NIGERIAN R2 HEATMAP
-            nigerian_r2_heatmap = LDheatmap(nigerian_r2_values, rsID_list, nigerian_r2_title)
-            plt.savefig("static/nigerian_r2.png")
+    ### NIGERIAN R2 HEATMAP
+    nigerian_r2_heatmap = LDheatmap(nigerian_r2_values, rsID_list, nigerian_r2_title)
+    plt.savefig("static/nigerian_r2.png")
 
-            ### JAPANESE R2 HEATMAP
-            japanese_r2_heatmap = LDheatmap(japanese_r2_values, rsID_list, japanese_r2_title)
-            plt.savefig("static/japanese_r2.png")
+    ### JAPANESE R2 HEATMAP
+    japanese_r2_heatmap = LDheatmap(japanese_r2_values, rsID_list, japanese_r2_title)
+    plt.savefig("static/japanese_r2.png")
 
 
-            ### BRITISH DPRIME HEATMAP
-            british_dprime_heatmap = LDheatmap(british_dprime_values, rsID_list, british_dprime_title)
-            plt.savefig("static/british_dprime.png")
+    ### BRITISH DPRIME HEATMAP
+    british_dprime_heatmap = LDheatmap(british_dprime_values, rsID_list, british_dprime_title)
+    plt.savefig("static/british_dprime.png")
 
-            ### NIGERIAN DPRIME HEATMAP
-            nigerian_dprime_heatmap = LDheatmap(nigerian_dprime_values, rsID_list, nigerian_dprime_title)
-            plt.savefig("static/nigerian_dprime.png")
+    ### NIGERIAN DPRIME HEATMAP
+    nigerian_dprime_heatmap = LDheatmap(nigerian_dprime_values, rsID_list, nigerian_dprime_title)
+    plt.savefig("static/nigerian_dprime.png")
 
-            ### JAPANESE DPRIME HEATMAP
-            japanese_dprime_heatmap = LDheatmap(japanese_dprime_values, rsID_list, japanese_dprime_title)
-            plt.savefig("static/japanese_dprime.png")
+    ### JAPANESE DPRIME HEATMAP
+    japanese_dprime_heatmap = LDheatmap(japanese_dprime_values, rsID_list, japanese_dprime_title)
+    plt.savefig("static/japanese_dprime.png")
 
 
 # Function to print out a file as a txt file for the user
